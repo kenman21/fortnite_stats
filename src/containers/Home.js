3e73a5fd49ade476b3712d803d8083cca5214643
Kenny Lehr <kenman9009@gmail.com>
2018-07-06 15:00:23 -0400

    Created form for player names

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 class Home extends React.Component {
 
+  state = {
+    name: ""
+  }
+
+  onChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   render() {
     return (
       <div>
-        <h1> {process.env.var} </h1>
+        <h1> Enter a player name </h1>
+        <form onSubmit={this.onSubmit}>
+          <input className='player-name' type='text' value={this.state.name} onChange={this.onChange}/>
+          <input type='submit'/>
+        </form>
       </div>
     )
   }
