e429747f08f58c0a929e4d94f47b6de36e19f529
Kenny Lehr <kenman9009@gmail.com>
2018-08-16 17:00:29 -0400

    working on player card aesthetic

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,9 +4,9 @@ class Player extends React.Component {
   render() {
     return(
       <div className="player">
+        <button onClick={() => this.props.removePlayer(this.props.index)} className="remove-player">X</button>
         <div className="player-header">
           <h1> {this.props.stats.epicUserHandle} </h1>
-          <button onClick={() => this.props.removePlayer(this.props.index)} className="remove-player">X</button>
         </div>
         <div className="stats">
           <h5> K/d: {this.props.stats.lifeTimeStats[11].value} </h5>
