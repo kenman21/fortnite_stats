94e2739f9e8d55815a46af3cc0c58f61f653adee
Kenny Lehr <kenman9009@gmail.com>
2018-08-16 17:48:39 -0400

    Fixed X button

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,15 +4,15 @@ class Player extends React.Component {
   render() {
     return(
       <div className="player">
-        <button onClick={() => this.props.removePlayer(this.props.index)} className="remove-player">X</button>
+        <p onClick={() => this.props.removePlayer(this.props.index)} className="remove-player">X</p>
         <div className="player-header">
           <h1> {this.props.stats.epicUserHandle} </h1>
         </div>
         <div className="stats">
-          <h5> K/d: {this.props.stats.lifeTimeStats[11].value} </h5>
-          <h5> Win %: {this.props.stats.lifeTimeStats[9].value} </h5>
-          <h5> Matches Played: {this.props.stats.lifeTimeStats[7].value} </h5>
-          <h5> Platform: {this.props.stats.platformNameLong} </h5>
+          <h1> K/d: {this.props.stats.lifeTimeStats[11].value} </h1>
+          <h1> Win %: {this.props.stats.lifeTimeStats[9].value} </h1>
+          <h1> Matches Played: {this.props.stats.lifeTimeStats[7].value} </h1>
+          <h1> Platform: {this.props.stats.platformNameLong} </h1>
         </div>
       </div>
     )
