e2e7a4cb846a0f8d10368e50b97ee5ab833ff542
Kenny Lehr <kenman9009@gmail.com>
2018-09-20 15:24:01 -0400

    Home Route added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './containers/Home.js'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/home" component={Home}/>
+        </div>
+      </Router>
     );
   }
 }
